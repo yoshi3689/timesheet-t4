1bcbcce82a8394190693d0e7ab6a4185dc348675
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2023-02-28 18:54:21 -0800

    add role-based authorization on the nav bar view and LG controller

--- Controllers/LabourGradeController.cs
+++ Controllers/LabourGradeController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using TimesheetApp.Models.TimesheetModels;
 
 namespace TimesheetApp.Controllers
 {
+    [Authorize(Roles = "HR,Admin")]
     public class LabourGradeController : Controller
     {
         private readonly ApplicationDbContext _context;
