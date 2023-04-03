befdec7ea911c8741a1fc604c0c5bfc8a210beb3
Dennis Kim <dkim334@my.bcit.ca>
2023-04-03 15:26:26 -0700

    logo bug fixed. shows up on all pages now. Create project page now allows project managers to input project Id

--- Models/TimesheetModels/Project.cs
+++ Models/TimesheetModels/Project.cs
@@ -8,39 +8,39 @@ using TimesheetApp.Controllers;
 
 namespace TimesheetApp.Models.TimesheetModels
 {
-    /// <summary>
-    /// Model of a project, in order to keep track of project specific information.
-    /// For the database.
-    /// </summary>
-    public class Project
-    {
-        [Key]
-        [Required]
-        [Display(Name = "Id")]
-        [Range(0, int.MaxValue, ErrorMessage = "Must be positive.")]
-        public int ProjectId { get; set; }
-        [Display(Name = "Name")]
-        [Required]
-        public string ProjectTitle { get; set; } = "";
-        [Required]
-        [Display(Name = "Manager")]
-        public string? ProjectManagerId { get; set; }
-        [Display(Name = "Asst. Manager")]
-        public string? AssistantProjectManagerId { get; set; }
-        [Display(Name = "Budget")]
-        public double TotalBudget { get; set; }
-        [Display(Name = "Actual Cost")]
-        public double ActualCost { get; set; }
-        public bool IsClosed { get; set; }
+  /// <summary>
+  /// Model of a project, in order to keep track of project specific information.
+  /// For the database.
+  /// </summary>
+  public class Project
+  {
+    [Key]
+    [Required]
+    [Display(Name = "Project Id")]
+    [Range(0, int.MaxValue, ErrorMessage = "Must be positive.")]
+    public int ProjectId { get; set; }
+    [Display(Name = "Name")]
+    [Required]
+    public string ProjectTitle { get; set; } = "";
+    [Required]
+    [Display(Name = "Manager")]
+    public string? ProjectManagerId { get; set; }
+    [Display(Name = "Asst. Manager")]
+    public string? AssistantProjectManagerId { get; set; }
+    [Display(Name = "Budget")]
+    public double TotalBudget { get; set; }
+    [Display(Name = "Actual Cost")]
+    public double ActualCost { get; set; }
+    public bool IsClosed { get; set; }
 
-        [ForeignKey("ProjectManagerId")]
-        [Display(Name = "Manager")]
-        public virtual ApplicationUser? ProjectManager { get; set; }
-        [ForeignKey("AssistantProjectManagerId")]
-        [Display(Name = "Asst. Manager")]
-        public virtual ApplicationUser? AssistantProjectManager { get; set; }
+    [ForeignKey("ProjectManagerId")]
+    [Display(Name = "Manager")]
+    public virtual ApplicationUser? ProjectManager { get; set; }
+    [ForeignKey("AssistantProjectManagerId")]
+    [Display(Name = "Asst. Manager")]
+    public virtual ApplicationUser? AssistantProjectManager { get; set; }
 
-        [InverseProperty("Project")]
-        public virtual ICollection<EmployeeProject> EmployeeProjects { get; } = new List<EmployeeProject>();
-    }
+    [InverseProperty("Project")]
+    public virtual ICollection<EmployeeProject> EmployeeProjects { get; } = new List<EmployeeProject>();
+  }
 }
