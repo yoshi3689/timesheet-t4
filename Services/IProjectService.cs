9e78a92b763bcdf2f0c85ddc7fe8bc585df7cd26
Yoshiyuki Nagai <yoshiyukinagai7@gmail.com>
2026-06-12 21:52:18 +0900

    feat: update IProjectService to use ProjectEmployeeDto

--- Services/IProjectService.cs
+++ Services/IProjectService.cs
@@ -15,6 +15,6 @@ public interface IProjectService
     Task<byte[]> GenerateReportAsync(int projectId);
     Task<byte[]> GenerateWeekReportAsync(int projectId);
     Task<byte[]> GeneratePCBACAsync(int projectId);
-    Task<List<object>> GetAllProjectEmployeesAsync(int projectId, string currentUserId);
+    Task<List<ProjectEmployeeDto>> GetAllProjectEmployeesAsync(int projectId, string currentUserId);
     Task<bool> AssignAssistantProjectManagerAsync(int projectId, string employeeNumber, string currentPmId);
 }
